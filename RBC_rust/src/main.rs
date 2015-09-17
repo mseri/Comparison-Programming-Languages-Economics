7eb401ac4ebdce29be2f2cbd1a3454cddbf4bdbc
Kevin Butler <haqkrs@gmail.com>
2015-09-17 07:23:37 +0100

    Invert data layout for outputs and some minor cleanup

--- RBC_rust/src/main.rs
+++ RBC_rust/src/main.rs
@@ -1,4 +1,3 @@
-// #![cfg_attr(test, feature(test))]
 extern crate time;
 extern crate num_cpus;
 extern crate scoped_threadpool;
@@ -7,9 +6,7 @@ use time::precise_time_s;
 use scoped_threadpool::Pool;
 use std::mem;
 
-///////////////////////////////////////////////////////////////////////////////
 // 1. Calibration
-///////////////////////////////////////////////////////////////////////////////
 const ALPHA: f64 = 0.33333333333;  // Elasticity of output w.r.t. capital
 const BETA: f64 = 0.95;           // Discount factor
 
@@ -27,17 +24,11 @@ static TRANSITIONS: [[f64; 5]; 5] = [[0.9727, 0.0273, 0.0000, 0.0000, 0.0000],
 const GRID_CAPITAL: usize = 17820;
 const GRID_PRODUCTIVITY: usize = 5;
 
-fn solve(print: bool) -> f64 {
-  /////////////////////////////////////////////////////////////////////////////
-  // 2. Steady State
-  /////////////////////////////////////////////////////////////////////////////
-
-    let thread_count = num_cpus::get();
-    let mut pool = Pool::new(thread_count as u32);
-
-    let capital_steady_state: f64 = (ALPHA * BETA).powf(1_f64 / (1_f64 - ALPHA));
-    let output_steady_state: f64 = capital_steady_state.powf(ALPHA);
-    let consumption_steady_state: f64 = output_steady_state - capital_steady_state;
+fn solve(pool: &mut Pool, print: bool) -> f64 {
+    // 2. Steady State
+    let capital_steady_state = (ALPHA * BETA).powf(1_f64 / (1_f64 - ALPHA));
+    let output_steady_state = capital_steady_state.powf(ALPHA);
+    let consumption_steady_state = output_steady_state - capital_steady_state;
 
     if print {
         println!("Output = {}, Capital = {}, Consumption = {}",
@@ -78,30 +69,27 @@ fn solve(print: bool) -> f64 {
     ];
 
     // 4. We pre-build output for each point in the grid
-    let output : Vec<[f64; GRID_PRODUCTIVITY]> = (0..GRID_CAPITAL).map(|capital_idx| {
-        let mut arr = [0.0; GRID_PRODUCTIVITY];
+    let outputs: [Vec<f64>; GRID_PRODUCTIVITY] = [
+        (0..GRID_CAPITAL).map(|capital_idx| PRODUCTIVITY[0] * grid_capital[capital_idx].powf(ALPHA)).collect(),
+        (0..GRID_CAPITAL).map(|capital_idx| PRODUCTIVITY[1] * grid_capital[capital_idx].powf(ALPHA)).collect(),
+        (0..GRID_CAPITAL).map(|capital_idx| PRODUCTIVITY[2] * grid_capital[capital_idx].powf(ALPHA)).collect(),
+        (0..GRID_CAPITAL).map(|capital_idx| PRODUCTIVITY[3] * grid_capital[capital_idx].powf(ALPHA)).collect(),
+        (0..GRID_CAPITAL).map(|capital_idx| PRODUCTIVITY[4] * grid_capital[capital_idx].powf(ALPHA)).collect(),
+    ];
 
-        for (productivity_idx, slot) in arr.iter_mut().enumerate() {
-            *slot = PRODUCTIVITY[productivity_idx] * grid_capital[capital_idx].powf(ALPHA)
-        }
-
-        arr
-    }).collect();
-
-  // 5. Main iteration
-  // TODO: one could implement a macro for the multiple declarations
-    let mut max_difference = 10_f64;
+    // 5. Main iteration
     const TOLERANCE: f64 = 0.0000001; // compiler warn: variable does not need to be mutable
 
     let mut iteration = 0;
+    let mut finished = false;
 
     // small array to split where the writes are going, so we don't need a mutex
     let mut diffs = [-100000.0; GRID_PRODUCTIVITY];
 
-    while max_difference > TOLERANCE {
+    while !finished {
         pool.scoped(|scoped| {
             for (expected_values, transitions) in expected_values.iter_mut()
-                                                                       .zip(TRANSITIONS.iter()) {
+                                                                 .zip(TRANSITIONS.iter()) {
                 // Only capture refs
                 let values = &values;
 
@@ -118,30 +106,26 @@ fn solve(print: bool) -> f64 {
         });
 
         pool.scoped(|scoped| {
-            for (productivity_idx, (((policies, value_fns), max_difference), expected_values)) in policies.iter_mut()
-                                                                         .zip(values.iter_mut())
-                                                                         .zip(diffs.iter_mut())
-                                                                         .zip(expected_values.iter())
-                                                                         .enumerate() {
-                *max_difference = -100000.0;
-
-                // Only capture refs
-                let output = &output;
-
+            for ((((policies, value_fns), max_diff), expected_values), outputs) in policies.iter_mut()
+                                                                                           .zip(values.iter_mut())
+                                                                                           .zip(diffs.iter_mut())
+                                                                                           .zip(expected_values.iter())
+                                                                                           .zip(outputs.iter()) {
                 scoped.execute(move || {
+                    *max_diff = -100000.0;
+
                     // We start from previous choice (monotonicity of policy function)
                     let mut grid_capital_next_period = 0;
 
                     for ((policy, output), value_fn) in policies.iter_mut()
-                                                                .zip(output.iter())
+                                                                .zip(outputs.iter())
                                                                 .zip(value_fns.iter_mut()) {
                         let mut value_high = -100000.0;
                         let mut capital_choice = grid_capital[0];
-                        let output_cache = &output[productivity_idx];
 
                         for capital_next_period in grid_capital_next_period..GRID_CAPITAL {
-                            let consumption = output_cache - &grid_capital[capital_next_period];
-                            let value_provisional = (1_f64 - BETA) * (consumption.ln()) +
+                            let consumption = output - &grid_capital[capital_next_period];
+                            let value_provisional = (1.0 - BETA) * (consumption.ln()) +
                                                     BETA *
                                                     expected_values[capital_next_period];
 
@@ -156,8 +140,8 @@ fn solve(print: bool) -> f64 {
 
                         let old = mem::replace(value_fn, value_high);
                         let diff = (old - value_high).abs();
-                        if diff > *max_difference {
-                            *max_difference = diff
+                        if diff > *max_diff {
+                            *max_diff = diff
                         }
 
                         *policy = capital_choice;
@@ -166,21 +150,19 @@ fn solve(print: bool) -> f64 {
             }
         });
 
-        max_difference = -100000.0;
-        for &diff in &diffs {
-            if diff > max_difference {
-                max_difference = diff
+        let mut max_diff = diffs[0];
+        for &diff in &diffs[1..] {
+            if diff > max_diff {
+                max_diff = diff
             }
         }
 
-        iteration += 1;
-        if print && (iteration % 10 == 0 || iteration == 1) {
-            println!("Iteration = {}, Sup Diff = {}", iteration, max_difference);
-        }
-    }
+        finished = max_diff <= TOLERANCE;
 
-    if print {
-        println!("Iteration = {}, Sup Diff = {}", iteration, max_difference);
+        iteration += 1;
+        if print && (iteration % 10 == 0 || iteration == 1 || finished) {
+            println!("Iteration = {}, Sup Diff = {}", iteration, max_diff);
+        }
     }
 
     policies[2][999]
@@ -190,11 +172,13 @@ fn main() {
     use std::env::args;
 
     let sample = args().any(|arg| arg == "--sample");
+    let thread_count = num_cpus::get();
+    let ref mut pool = Pool::new(thread_count as u32);
 
     if sample {
         let mut samples = (0..5).map(|i| {
             let cpu0 = precise_time_s();
-            let result = solve(false);
+            let result = solve(pool, false);
             let cpu1 = precise_time_s();
 
             assert_eq!(result, 0.1465491436956954);
@@ -208,7 +192,7 @@ fn main() {
         println!("\nMedian time is = {}", samples[2]);
     } else {
         let cpu0 = precise_time_s();
-        let result = solve(true);
+        let result = solve(pool, true);
         let cpu1 = precise_time_s();
 
         println!("My check = {}\n", result);
@@ -216,21 +200,3 @@ fn main() {
         println!("Elapsed time is = {}", cpu1 - cpu0);
     }
 }
-
-
-// FIXME: Can't use bench as the loop is too slow (takes 8mins to get samples)
-// #[cfg(test)]
-// mod test {
-//     extern crate test;
-//     use self::test::Bencher;
-//     use super::solve;
-
-//     #[bench]
-//     fn bench(b: &mut Bencher) {
-//         b.iter(|| {
-//             // use `test::black_box` to prevent compiler optimizations from disregarding
-//             // unused values
-//             test::black_box(solve(false));
-//         });
-//     }
-// }
